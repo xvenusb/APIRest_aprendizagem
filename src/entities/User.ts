a1c9400b3dc4ee2e3c3dffec9c60a61311e31a7b
luminuszz <luminuszz43@gmail.com>
2024-10-02 23:52:32 -0300

    feat: ajustes de sintaxe, amanhã te explico

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -2,21 +2,14 @@ import { uuid } from "uuidv4";
 
 export class User {
     public readonly id: string;
-
     public name: string | undefined;
     public email: string | undefined;
     public password: string | undefined;
- 
+
     //para criar uma nova entidade. preenchimento automático do usuário
     constructor(props: Omit<User, 'id'>, id?: string) {
         Object.assign(this, props);
-
-        if (!id) {
-            this.id = uuid();
-        } else {
-            this.id = id;
-        }
-
+        this.id = id ?? uuid();
     }
-        
+
 }
--- src/repositories/IUsersRepository.ts
+++ src/repositories/IUsersRepository.ts
@@ -1,7 +1,6 @@
 import { User } from "../entities/User";
 
 export interface IUsersRepository {
-    findByEmail (email: string): Promise<User>;
-
+    findByEmail (email: string): Promise<User | null>;
     save(user: User): Promise<void>;
 }
